0684b6524f63cddce729491e6f22a32b88e027eb
golmschenk <greg@olmschenk.com>
2023-06-20 00:57:01 -0400

    Test without tests being a package

--- tests/test_library_function.py
+++ tests/test_library_function.py
@@ -4,3 +4,4 @@ from scikit_build_core_minimal_shared_library_example import library_function
 def test_library_function_returns_integer():
     return_value = library_function()
     assert return_value == 4
+
