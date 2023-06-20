d720e420eb2f736da22563103c72413b4a258654
golmschenk <greg@olmschenk.com>
2023-06-20 01:31:35 -0400

    Simplify the CDLL library load

--- scikit_build_core_minimal_shared_library_example/__init__.py
+++ scikit_build_core_minimal_shared_library_example/__init__.py
@@ -10,24 +10,15 @@ library_directory = module_parent_directory
 platform_system_name = platform.system()
 if platform_system_name == 'Darwin':
     library_path = library_directory.joinpath('liblibrary_example.dylib')
-    if not library_path.exists():
-        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), str(library_path.absolute()))
-    c_library = CDLL(str(library_path))
 elif platform_system_name == 'Linux':
     library_path = library_directory.joinpath('liblibrary_example.so')
-    if not library_path.exists():
-        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), str(library_path.absolute()))
-    c_library = CDLL(str(library_path))
-elif platform_system_name == 'Windows':
-    library_path = library_directory.joinpath('library_example.dll')
-    if not library_path.exists():
-        library_path = library_directory.joinpath('liblibrary_example.dll')
-    if not library_path.exists():
-        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), str(library_path.absolute()))
-    c_library = CDLL(str(library_path))
 else:
     raise ValueError(f'Platform system {platform_system_name} is not supported.')
 
+if not library_path.exists():
+    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), str(library_path.absolute()))
+
+c_library = CDLL(str(library_path))
 library_function = c_library.library_function
 library_function.return_type = c_int
 
